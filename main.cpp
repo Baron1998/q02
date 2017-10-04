808d4f785991c0ea1610fdbcff2489837872b166
Baron1998 <abaron3@mail.csuchico.edu.com>
2017-10-04 10:48:48 -0700

    Answer for quiz 2

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
   vector <vector <string> > wordGrid;
-
+    vector<string> w;
   /*
      do not edit this space
 
@@ -19,6 +19,34 @@ int main()
 
   //PUT YOUR CODE BELOW THIS LINE
   //that prints each element of the matrix in the
+    
+   w.push_back("home");
+   w.push_back("car");
+   w.push_back("cat");
+   w.push_back("paper");
+   w.push_back("amazing");
+
+   wordGrid.push_back(w);
+
+   w.clear();
+
+   w.push_back("run");
+   w.push_back("swim");
+   w.push_back("football");
+   w.push_back("waterball");
+
+   wordGrid.push_back(w);
+
+   for(int i=0; i < wordGrid.size();i++)
+        {
+          for(int r=0; r < w.size();r++)
+         {
+            cout<<"Row "<<i<<"Column "<<r<<"is  "<<wordGrid[i][r]<<endl;
+           }
+        cout<<endl;
+        }
+
+
   //following format:
   //Row _ Column _ is _
   //the first and second blanks should be replaced with the coordinates
